979c10557e4656373e2306cf46042e8c820b6d30
Tim Clark <timeeeee@gmail.com>
2026-04-08 23:51:51 +0000

    initial commit - rust project, boxes for collision detection

---
+++ README.md
@@ -0,0 +1,3 @@
+# Generate Lichtenberg figures
+
+Strategy: Start with one stationary particle. Add a moving particle which random walks until it encounters a stationary particle. At this point, it becomes stationary, and adds a line from itself to the particle that it encountered.
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use array2d::Array2D;
+
+const WALK_STEP_LENGTH: f32 = 1.0;
+
+const WORLD_SIZE: i32 = 256;
+const BOX_SIZE: i32 = 4;
+
+struct Point {
+    x: f32,
+    y: f32
+}
+
+fn main() {
+    let mut boxes = Array2D::filled_by_row_major(Vec::<Point>::new, 64, 64);
+    let mut Vec::<Point>::new();
+}
